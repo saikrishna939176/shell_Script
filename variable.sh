9052f14ec83b5a2fecee8502ec911d791a7436dd
Sai Devabhakthuni <sai.devabhakthuni@gspann.com>
2026-09-22 15:29:30 +0530

    add

--- variable.sh
+++ variable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Welcone to script"
-# num1 = $1
-# num2 = $2
+ num1 = $1
+ num2 = $2
 
-Add = $(( $1  +  $2 ))
-echo "addition of $1 and $2 :  ${Add}"
+Add = $(( $num1  +  $num2 ))
+echo "addition of $num1 and $num2 :  ${Add}"
