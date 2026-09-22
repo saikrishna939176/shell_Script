1067b00185c4b08d9b62d0215052b42868bad6e0
Sai Devabhakthuni <sai.devabhakthuni@gspann.com>
2026-09-22 15:42:43 +0530

    add

--- variable.sh
+++ variable.sh
@@ -6,3 +6,12 @@ num2=$2
 
 Add=$(($num1+$num2))
 echo "addition of $num1 and $num2 :  ${Add}"
+
+
+if [ $Add -gt 20 ]
+then
+    echo "add value is greater than 20"
+else
+    echo "add value is not greather than 20"
+fi
+
